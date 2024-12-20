40974dd544972a1f89725022e6cdf9edcf2fe442
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-12-21 04:31:37 +0800

    Update node_info.sh

--- node_info.sh
+++ node_info.sh
@@ -13,5 +13,6 @@ yellow() {
 clear
 yellow "节点信息："
 echo
-cat list.txt
+[[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
+cat $WORKDIR/list.txt
 echo
