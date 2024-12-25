0b029ee08cb5f49b2f58fc1bd5cfe2449e3ee006
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-12-25 23:43:34 +0800

    Update init_serv00.sh

--- init_serv00.sh
+++ init_serv00.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-    echo "" >null
+
+echo "此操作将删除所有定时任务、结束当前用户所有进程、并删除当前用户主目录下的所有文件和文件夹、包括隐藏文件"
+read -p "你确定要继续初始化吗? (y/n): " confirm
+
+if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
+    echo "" > null
     crontab null
     rm null
     local user=$(whoami)
     pkill -kill -u $user
     rm -rf ~/* ~/.* 2>/dev/null
-    echo "已删除所有定时任务"
-    echo "已结束当前用户所有进程"
-    echo "已删除当前用户主目录下的所有文件和文件夹，包括隐藏文件"
     echo
     echo "初始化完成"
+else
+    echo
+    echo "初始化操作已取消"
+fi
