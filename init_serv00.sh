0a9f8830919c135bb92722e71d1b454dea61ce79
co2f2e <48397609+co2f2e@users.noreply.github.com>
2024-12-26 17:58:17 +0800

    Update init_serv00.sh

--- init_serv00.sh
+++ init_serv00.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+clear
 echo "此操作将删除所有定时任务、结束当前用户所有进程、并删除当前用户主目录下的所有文件和文件夹、包括隐藏文件、退出SSH连接"
-
+echo
 while true; do
     read -p "你确定要继续初始化吗? (y/n): " confirm
     if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
@@ -15,9 +15,11 @@ while true; do
         echo "初始化完成"
         break  
     elif [[ "$confirm" == "n" || "$confirm" == "N" ]]; then
+        echo
         echo "初始化操作已取消"
         break  
     else
+        echo
         echo "无效输入，请输入'y'或'n'"
     fi
 done
