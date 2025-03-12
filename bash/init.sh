83b3de73e8019e386d5882e968bfb85649d31591
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-03-12 15:01:25 +0800

    Update init.sh

--- bash/init.sh
+++ bash/init.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-echo "" > null
-crontab null
-rm null
+echo "" > /tmp/null
+crontab /tmp/null
+rm -f /tmp/null
 user=$(whoami)
-pgrep -u $user | grep -v $(pgrep -u $user sshd) | xargs kill -9
-rm -rf ~/* ~/.* 2>/dev/null
+for pid in $(pgrep -u $user); do
+    if ! ps -fp $pid | grep -q '[s]shd'; then
+        kill -9 $pid
+    fi
+done
+find ~ -mindepth 1 ! -name ".ssh" ! -name ".bashrc" ! -name ".profile" -exec rm -rf {} \; 2>/dev/null
