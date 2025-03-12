3113e026d314f14a27fe56e6d5f1d28665e7dfe7
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-03-12 17:31:21 +0800

    Update uninstall_singbox.sh

--- bash/uninstall_singbox.sh
+++ bash/uninstall_singbox.sh
@@ -15,7 +15,7 @@ HOSTNAME=$(hostname)
 PROCESS_NAME="singbox"
 [[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
 
-pkill -9 "$PROCESS_NAME"
+pkill -x "$PROCESS_NAME"
 sleep 1
 
 if pgrep "$PROCESS_NAME" > /dev/null; then
