a31c16bce392f951dd2ff0a8f74bd021c75c6fb3
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-06-09 04:40:59 +0800

    Update singbox_uninstall.sh

--- bash/singbox_uninstall.sh
+++ bash/singbox_uninstall.sh
@@ -16,18 +16,48 @@ HOSTNAME=$(hostname)
 PROCESS_NAME="singbox"
 [[ "$HOSTNAME" == "s1.ct8.pl" ]] && WORKDIR="domains/${USERNAME}.ct8.pl/logs" || WORKDIR="domains/${USERNAME}.serv00.net/logs"
 
+manage_ports() {
+    local udp_ports tcp_ports udp_port
+
+    tcp_ports=$(devil port list | awk '$2=="tcp"{print $1}')
+    for port in $tcp_ports; do
+        devil port remove "$port"
+    done
+
+    udp_ports=$(devil port list | awk '$2=="udp"{print $1}')
+
+    if [[ $(echo "$udp_ports" | wc -l) -gt 1 ]]; then
+        udp_port=$(echo "$udp_ports" | head -n 1)
+        echo "$udp_ports" | tail -n +2 | while read -r port; do
+            devil port remove "$port"
+        done
+    elif [[ $(echo "$udp_ports" | wc -l) -eq 1 ]]; then
+        udp_port=$udp_ports
+    else
+        udp_port=""
+    fi
+
+    if [[ -n "$udp_port" ]]; then
+        echo "已删除所有TCP端口，只保留了一个UDP端口"
+    else
+        echo "已删除所有端口"
+    fi
+}
+
+
 pkill -x "$PROCESS_NAME"
 sleep 1
 
 if pgrep -x "$PROCESS_NAME" > /dev/null; then
-    red "❌ $PROCESS_NAME 卸载失败，请重试！"
+    red "❌ $PROCESS_NAME 卸载失败，请重试"
     exit 0
 else
     echo "" > null
     crontab null
     rm null
     [ -d "$WORKDIR" ] && rm -r "$WORKDIR"
-    yellow "✅ $PROCESS_NAME 卸载成功，已删除相关文件！"
+    yellow "✅ $PROCESS_NAME 卸载成功，已删除相关文件"
+    manage_ports
     exit 0
 fi
 
