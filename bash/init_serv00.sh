49d0dccf1e0e44b1ab462cac03a64b20d79cf90e
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-06-09 04:46:48 +0800

    Update init_serv00.sh

--- bash/init_serv00.sh
+++ bash/init_serv00.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 clear
-echo "此操作将删除所有定时任务、结束当前用户所有进程、并删除当前用户主目录下的所有文件和文件夹、包括隐藏文件、退出SSH连接"
+echo "此操作将删除所有申请的端口、定时任务、结束当前用户所有进程、并删除当前用户主目录下的所有文件和文件夹、包括隐藏文件、退出SSH连接"
 echo
+
+delete_ports() {
+    local ports
+
+    ports=$(devil port list | awk 'NR>1 {print $1}')
+
+    for port in $ports; do
+        devil port remove "$port"
+    done
+
+    echo "已删除所有端口"
+}
+
 while true; do
     read -p "你确定要继续初始化吗? (y/n): " confirm
     if [[ "$confirm" == "y" || "$confirm" == "Y" ]]; then
+        delete_ports
         echo "" > null
         crontab null
         rm null
